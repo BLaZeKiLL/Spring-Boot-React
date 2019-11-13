a7006cd728acffc8fc9b80197eec3c9da8685d01
Devashish Lal <laldevashish@gmail.com>
2019-11-13 20:56:49 +0530

    Node plugin setup

--- src/main/java/org/blazekill/springreact/controllers/DummyController.java
+++ src/main/java/org/blazekill/springreact/controllers/DummyController.java
@@ -1,5 +1,14 @@
 package org.blazekill.springreact.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller("/api")
 public class DummyController {
+
+    @GetMapping("/booty")
+    public void giveBooty() {
+
+    }
 
 }
